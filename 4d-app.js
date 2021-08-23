d1867c2288e7b918c1cb6b1a75ce5be13ae84719
Graham Preston <grahampreston@comcast.net>
2021-08-22 23:54:33 -0400

    Added 4d and edges

--- 4d-app.js
+++ 4d-app.js
@@ -54,7 +54,7 @@ const matrixMultiplication = (a, b) => {
 };
 
 const generateCube = (n) => {
-	const end = 2 ** 3;
+	const end = 2 ** 4;
 	const points = [];
 	for (let i = 0; i < end; i++) {
 		const binary = i.toString(2).padStart(end.toString(2).length - 1, 0);
@@ -67,19 +67,40 @@ const generateCube = (n) => {
 // Rotations do not necessarily follow the right-hand-rule
 const Rotation = Object.freeze({
 	xy: theta => [
-		[Math.cos(theta), -Math.sin(theta), 0],
-		[Math.sin(theta), Math.cos(theta), 0],
-		[0, 0, 1]
+		[Math.cos(theta), -Math.sin(theta), 0, 0],
+		[Math.sin(theta), Math.cos(theta), 0, 0],
+		[0, 0, 1, 0],
+		[0, 0, 0, 1]
 	],
 	xz: theta => [
-		[Math.cos(theta), 0, -Math.sin(theta)],
-		[0, 1, 0],
-		[Math.sin(theta), 0, Math.cos(theta)]
+		[Math.cos(theta), 0, -Math.sin(theta), 0],
+		[0, 1, 0, 0],
+		[Math.sin(theta), 0, Math.cos(theta), 0],
+		[0, 0, 0, 1]
 	],
-	yz: theta => [	
-		[1, 0, 0],
-		[0, Math.cos(theta), -Math.sin(theta)],
-		[0, Math.sin(theta), Math.cos(theta)]
+	xw: theta => [
+		[Math.cos(theta), 0, 0, -Math.sin(theta)],
+		[0, 1, 0, 0],
+		[0, 0, 1, 0],
+		[Math.sin(theta), 0, 0, Math.cos(theta)],
+	],
+	yz: theta => [
+		[1, 0, 0, 0],
+		[0, Math.cos(theta), -Math.sin(theta), 0],
+		[0, Math.sin(theta), Math.cos(theta), 0],
+		[0, 0, 0, 1]
+	],
+	yw: theta => [
+		[1, 0, 0, 0],
+		[0, Math.cos(theta), 0, -Math.sin(theta)],
+		[0, 0, 1, 0],
+		[0, Math.sin(theta), 0, Math.cos(theta)]
+	],
+	zw: theta => [
+		[1, 0, 0, 0],
+		[0, 1, 0, 0],
+		[0, 0, Math.cos(theta), -Math.sin(theta)],
+		[0, 0, Math.sin(theta), Math.cos(theta)]
 	]
 });
 
@@ -105,12 +126,15 @@ const draw = (...args) => {
 
 	const projection = [];
 	for (const point of points) {
-		const pointMatrix = [[point[0]], [point[1]], [point[2]]];
+		const pointMatrix = [[point[0]], [point[1]], [point[2]], [point[3]]];
 		const theta = window.performance.now() / 1000;
 		let newPointMatrix = matrixMultiplication(Rotation.xy(theta), pointMatrix);
 		newPointMatrix = matrixMultiplication(Rotation.xz(theta), newPointMatrix);
+		newPointMatrix = matrixMultiplication(Rotation.xw(theta), newPointMatrix);
 		newPointMatrix = matrixMultiplication(Rotation.yz(theta), newPointMatrix);
-		const newPoint = [newPointMatrix[0][0], newPointMatrix[1][0], newPointMatrix[2][0]];
+		newPointMatrix = matrixMultiplication(Rotation.yw(theta), newPointMatrix);
+		newPointMatrix = matrixMultiplication(Rotation.zw(theta), newPointMatrix);
+		const newPoint = [newPointMatrix[0][0], newPointMatrix[1][0], newPointMatrix[2][0], newPointMatrix[3][0]];
 		projection.push(newPoint);
 	}
 
@@ -125,7 +149,8 @@ const draw = (...args) => {
 			if (a[0] === b[0]) matches++;
 			if (a[1] === b[1]) matches++;
 			if (a[2] === b[2]) matches++;
-			if (matches < 2) continue;
+			if (a[3] === b[3]) matches++;
+			if (matches < 3) continue;
 
 			pairs.push([i, j]);
 		}
